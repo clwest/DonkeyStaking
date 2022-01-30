f289298e1ea88a3381b2c3d827a690b71e84817f
clwest <chriswest@chriss-mbp.lan>
2022-01-29 23:10:08 -0700

    Aped into curve

---
+++ scripts/stake.py
@@ -0,0 +1,29 @@
+import re
+from brownie import Contract, accounts, interface
+from brownie_tokens import MintableForkToken
+
+def main():
+    dai_addr = "0x6b175474e89094c44da98b954eedeac495271d0f"
+    usdc_addr = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    registry_addr = "0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c"
+        
+
+    amount = 100_000 * 10**18
+    dai = MintableForkToken(dai_addr)
+    dai._mint_for_testing(accounts[0], amount)
+
+    registry = Contract(registry_addr)
+    pool_addr = registry.find_pool_for_coins(
+            dai_addr, usdc_addr)
+    pool = Contract(pool_addr)
+
+    dai.approve(pool_addr, amount, {"from": accounts[0]})
+    pool.add_liquidity([amount, 0, 0], 0, {'from': accounts[0]})
+
+    gauges = registry.get_gauges(pool_addr)
+    gauge_addr = gauges[0][0]
+    gauge_contract = interface.LiquidityGauge(gauge_addr)
+
+    lp_token = MintableForkToken(gauge_contract.lp_token())
+    lp_token.approve(gauge_addr, amount, {"from": accounts[0]})
+    gauge_contract.deposit(lp_token.balanceOf(accounts[0]), {"from": accounts[0]})
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from web3 import Web3
 from scripts.helpful_scripts import get_account
-from brownie import MockERC20
+from brownie_tokens import MintableForkToken
+from brownie import MockERC20, Contract, network
 
+@pytest.fixture(scope="function", autouse=True)
+def isolate(fn_isolation):
+    # perform a chain rewind after completing each test, to ensure proper isolation
+    # https://eth-brownie.readthedocs.io/en/v1.10.3/tests-pytest-intro.html#isolation-fixtures
+    pass
 
 @pytest.fixture
 def amount_staked():
@@ -13,3 +19,51 @@ def random_erc20():
     account = get_account()
     erc20 = MockERC20.deploy({"from": account})
     return erc20
+
+ # Referance Account A
+@pytest.fixture(scope="module")
+def alice(accounts):
+    return accounts[0]
+    # Referance Account B
+@pytest.fixture(scope="module")
+def bob(accounts):
+    return accounts[1]
+
+# Pulls contracts if they exist otherwise makes API call to etherscan
+def load_contract(addr):
+    try:
+        cont = Contract(addr)
+    except ValueError:
+        cont = Contract.from_explorer(addr)
+    return cont
+
+
+
+# Gets the curve registry contract
+@pytest.fixture(scope="module")
+def registry():
+    return load_contract("0x90E00ACe148ca3b23Ac1bC8C240C2a7Dd9c2d7f5")
+
+# calls the tripool from registry
+@pytest.fixture(scope="module")
+def tripool(registry):
+    return load_contract(registry.pool_list(0))
+
+
+@pytest.fixture(scope="module")
+def tripool_lp_token(registry, tripool):
+    return load_contract(registry.get_lp_token(tripool))
+
+
+# Mints tokens and deposits them into the first contract of the pool
+@pytest.fixture(scope="module")
+def tripool_funded(registry, alice, tripool):
+    dai_addr = registry.get_coins(tripool)[0]
+    dai = MintableForkToken(dai_addr)
+    amount = 1e21
+    dai.approve(tripool, amount, {"from": alice})
+    dai._mint_for_testing(alice, amount)
+
+    amounts = [amount, 0, 0]
+    tripool.add_liquidity(amounts, 0, {"from": alice})
+    return tripool
